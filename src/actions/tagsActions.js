c8fde16d1393c29fb2fcddf5ce701c60ab0636cb
Michał Kotfica <kotvitz17@gmail.com>
2024-04-04 00:46:18 +0200

    create sorting and pagination

--- src/actions/tagsActions.js
+++ src/actions/tagsActions.js
@@ -1,34 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const FETCH_TAGS_REQUEST = 'FETCH_TAGS_REQUEST';
-export const FETCH_TAGS_SUCCESS = 'FETCH_TAGS_SUCCESS';
-export const FETCH_TAGS_FAILURE = 'FETCH_TAGS_FAILURE';
+export const FETCH_TAGS_REQUEST = "FETCH_TAGS_REQUEST";
+export const FETCH_TAGS_SUCCESS = "FETCH_TAGS_SUCCESS";
+export const FETCH_TAGS_FAILURE = "FETCH_TAGS_FAILURE";
 
 export const fetchTagsRequest = () => ({
   type: FETCH_TAGS_REQUEST,
 });
 
-export const fetchTagsSuccess = tags => ({
+export const fetchTagsSuccess = (tags) => ({
   type: FETCH_TAGS_SUCCESS,
   payload: tags,
 });
 
-export const fetchTagsFailure = error => ({
+export const fetchTagsFailure = (error) => ({
   type: FETCH_TAGS_FAILURE,
   payload: error,
 });
 
-export const fetchTags = () => {
-    return dispatch => {
-      dispatch(fetchTagsRequest());
-      axios
-        .get('https://api.stackexchange.com/2.3/tags?site=stackoverflow')
-        .then(response => {
-          const tags = response.data.items;
-          dispatch(fetchTagsSuccess(tags));
-        })
-        .catch(error => {
-          dispatch(fetchTagsFailure(error));
-        });
-    };
+export const fetchTags = (params) => {
+  return async (dispatch) => {
+    dispatch(fetchTagsRequest());
+    try {
+      const response = await axios.get(
+        "https://api.stackexchange.com/2.3/tags?site=stackoverflow",
+        { params }
+      );
+      dispatch(fetchTagsSuccess(response.data.items));
+    } catch (error) {
+      dispatch(fetchTagsFailure(error.message));
+    }
   };
+};
--- src/components/TagBrowser.js
+++ src/components/TagBrowser.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   CircularProgress,
@@ -7,16 +7,65 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  Button,
+  IconButton,
 } from "@mui/material";
 import { fetchTags } from "../actions/tagsActions";
+import { ArrowUpward, ArrowDownward } from "@mui/icons-material";
 
 const TagBrowser = () => {
   const dispatch = useDispatch();
   const { tags, loading, error } = useSelector((state) => state.tags);
+  const [pagination, setPagination] = useState({ page: 1, perPage: 10 });
+  const [sortedColumn, setSortedColumn] = useState(null);
+  const [sortOrder, setSortOrder] = useState(null);
 
   useEffect(() => {
     dispatch(fetchTags());
   }, [dispatch]);
+
+  const handleSort = (column) => {
+    if (sortedColumn === column) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortedColumn(column);
+      setSortOrder("asc");
+    }
+    setPagination({ ...pagination, page: 1 });
+  };
+
+  const arrowDirection = (column) => {
+    if (sortedColumn === column) {
+      return sortOrder === "asc" ? <ArrowUpward /> : <ArrowDownward />;
+    }
+    return <ArrowDownward />;
+  };
+
+  const sortedTags = tags.sort((a, b) => {
+    if (sortOrder === "asc") {
+      return a[sortedColumn] > b[sortedColumn] ? 1 : -1;
+    } else {
+      return a[sortedColumn] < b[sortedColumn] ? 1 : -1;
+    }
+  });
+
+  const startIndex = (pagination.page - 1) * pagination.perPage;
+  const endIndex = startIndex + pagination.perPage;
+  const paginatedTags = sortedTags.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(sortedTags.length / pagination.perPage);
+
+  const handlePrevPage = () => {
+    if (pagination.page > 1) {
+      setPagination({ ...pagination, page: pagination.page - 1 });
+    }
+  };
+
+  const handleNextPage = () => {
+    if (pagination.page < totalPages) {
+      setPagination({ ...pagination, page: pagination.page + 1 });
+    }
+  };
 
   if (loading) return <CircularProgress />;
 
@@ -27,12 +76,22 @@ const TagBrowser = () => {
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Tag</TableCell>
-            <TableCell>Count</TableCell>
+            <TableCell>
+              Tag
+              <IconButton onClick={() => handleSort("name")}>
+                {arrowDirection("name")}
+              </IconButton>
+            </TableCell>
+            <TableCell>
+              Count
+              <IconButton onClick={() => handleSort("count")}>
+                {arrowDirection("count")}
+              </IconButton>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {tags.map((tag) => (
+          {paginatedTags.map((tag) => (
             <TableRow key={tag.name}>
               <TableCell>{tag.name}</TableCell>
               <TableCell>{tag.count}</TableCell>
@@ -40,6 +99,15 @@ const TagBrowser = () => {
           ))}
         </TableBody>
       </Table>
+      <Button onClick={handlePrevPage} disabled={pagination.page === 1}>
+        Previous Page
+      </Button>
+      <Button
+        onClick={handleNextPage}
+        disabled={pagination.page === totalPages}
+      >
+        Next Page
+      </Button>
     </div>
   );
 };
